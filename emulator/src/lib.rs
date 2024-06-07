43f5705ab6d790af7b75cd889dddfe2ace968ff4
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-06-07 10:41:06 -0300

    feat: :sparkles: Add all 36 chip8 instruction evaluation

--- emulator/src/lib.rs
+++ emulator/src/lib.rs
@@ -8,7 +8,6 @@ const STACK_SIZE: usize = 16;
 const KEYS_SIZE: usize = 16;
 const RAM_START_ADDR: u16 = 0x200;
 
-
 pub struct Emulator {
     ram: [u8; RAM_SIZE],
     v_reg: [u8; REGISTER_SIZE],
@@ -18,7 +17,7 @@ pub struct Emulator {
     pc: u16, // program counter
     sp: u8,  // stack pointer
     stack: [u16; STACK_SIZE],
-    keys: [u8; KEYS_SIZE], // array for storing keyboard input
+    keys: [bool; KEYS_SIZE], // array for storing keyboard input
     // array for storing pixels stater
     // because a pixel is either on or off, using bool is fine
     display: [bool; DISPLAY_SIZE],
@@ -35,7 +34,7 @@ impl Emulator {
             sp: 0,
             pc: RAM_START_ADDR,
             stack: [0; STACK_SIZE],
-            keys: [0; KEYS_SIZE],
+            keys: [false; KEYS_SIZE],
             display: [false; DISPLAY_SIZE],
         }
     }
@@ -68,15 +67,241 @@ impl Emulator {
         let instruction = self.get_next_instruction();
 
         // decode the instruction
-        let decoded_ins: [u8; 4] = [
+        let decoded_instruction: [u8; 4] = [
             ((instruction >> 12) & 0xF000) as u8, // first 4 bits
-            ((instruction >> 4)  & 0x0F00) as u8,
-            ((instruction >> 8)  & 0x00F0) as u8,
-            ((instruction >> 0)  & 0x000F) as u8, // last 4 bits
+            ((instruction >> 4) & 0x0F00) as u8,
+            ((instruction >> 8) & 0x00F0) as u8,
+            ((instruction >> 0) & 0x000F) as u8, // last 4 bits
         ];
 
-        match decoded_ins {
-            _ => unreachable!("Error. Unknown instruction: 0x{:x}", instruction)
+        match decoded_instruction {
+            // NOOP
+            [0, 0, 0, 0] => (),
+            // CLS -> clear the display
+            [0, 0, 0xE, 0] => {
+                self.display = [false; DISPLAY_SIZE];
+            }
+            // RET -> return from a subroutine
+            [0, 0, 0xE, 0xE] => {
+                self.pc = self.pop();
+            }
+            // JMP ADDR -> jump to nnn
+            [1, ..] => {
+                let nnn = instruction & 0x0FFF;
+                self.pc = nnn;
+            }
+            // CALL ADDR -> call subroutine at nnn
+            [2, ..] => {
+                let nnn = instruction & 0x0FFF;
+                self.push(self.pc);
+                self.pc = nnn;
+            }
+            // SE Vx, byte -> Skip next instruction if Vx == kk
+            [3, x, ..] => {
+                let kk = (instruction & 0xFF) as u8;
+                if self.v_reg[x as usize] == kk {
+                    self.pc += 2;
+                }
+            }
+            // SNE Vx, byte -> Skip next instruction if Vx != kk
+            [4, x, ..] => {
+                let kk = (instruction & 0xFF) as u8;
+                if self.v_reg[x as usize] != kk {
+                    self.pc += 2;
+                }
+            }
+            // SE, Vx, Vy -> Skip next instruction if Vx == Vy
+            [5, x, y, 0] => {
+                if self.v_reg[x as usize] == self.v_reg[y as usize] {
+                    self.pc += 2;
+                }
+            }
+            // LD Vx, byte -> Set Vx == kk
+            [6, x, ..] => {
+                let kk = (instruction & 0xFF) as u8;
+                self.v_reg[x as usize] = kk;
+            }
+            // ADD Vx, byte -> Set Vx = Vx + kk
+            [7, x, ..] => {
+                let kk = (instruction & 0xFF) as u8;
+                self.v_reg[x as usize] += kk;
+            }
+            // LD Vx, Vy -> Set Vx = Vy
+            [8, x, y, 0] => {
+                self.v_reg[x as usize] = self.v_reg[y as usize];
+            }
+            // OR Vx, Vy -> Set Vx = Vx OR Vy
+            [8, x, y, 1] => {
+                self.v_reg[x as usize] |= self.v_reg[y as usize];
+            }
+            // AND Vx, Vy -> Set Vx = Vx AND Vy
+            [8, x, y, 2] => {
+                self.v_reg[x as usize] &= self.v_reg[y as usize];
+            }
+            // XOR Vx, Vy -> Set Vx = Vx XOR Vy
+            [8, x, y, 3] => {
+                self.v_reg[x as usize] ^= self.v_reg[y as usize];
+            }
+            // ADD Vx, Vy -> Set Vx = Vx + Vy, Set VF = carry
+            [8, x, y, 4] => {
+                let (x, y) = (x as usize, y as usize);
+                let (vx, carry) = self.v_reg[x].overflowing_add(self.v_reg[y]);
+                self.v_reg[0xF] = carry as u8;
+                self.v_reg[x] = vx;
+            }
+            // SUB Vx, Vy -> SET Vx = Vx - Vy, SET VF = NOT borrow
+            [8, x, y, 5] => {
+                let (x, y) = (x as usize, y as usize);
+                let (vx, borrow) = self.v_reg[x].overflowing_sub(self.v_reg[y]);
+                self.v_reg[0xF] = (!borrow) as u8;
+                self.v_reg[x] = vx;
+            }
+            // SHR, Vx {, Vy} -> Set Vx = Vx SHR 1;
+            [8, x, _, 6] => {
+                let x = x as usize;
+                let lsb = self.v_reg[x] & 1; // least significant bit of Vx
+                self.v_reg[0xF] = lsb;
+                self.v_reg[x] >>= 1; // divide Vx by 2 (right bit shift)
+            }
+            // SUBN, Vx, Vy -> Set Vx = Vy - Vx, set VF = NOT borrow
+            [8, x, y, 7] => {
+                let (x, y) = (x as usize, y as usize);
+                let (vx, borrow) = self.v_reg[y].overflowing_sub(self.v_reg[x]);
+                self.v_reg[0xF] = (!borrow) as u8;
+                self.v_reg[x] = vx;
+            }
+            // SHL, Vx {, Vy} -> Set Vx = Vx SHL 1
+            [8, x, _, 0xE] => {
+                let x = x as usize;
+                let msb = (self.v_reg[x] >> 7) & 1; // most significant bit of Vx
+                self.v_reg[0xF] = msb;
+                self.v_reg[x] <<= 1; // multiply Vx by 2 (left bit shift)
+            }
+            // SNE, Vx, Vy -> Skip next instruction if Vx != Vy
+            [9, x, y, 0] => {
+                if self.v_reg[x as usize] != self.v_reg[y as usize] {
+                    self.pc += 2;
+                }
+            }
+            // LD I, addr -> Set I Register = nnn;
+            [0xA, ..] => {
+                let nnn = instruction & 0xFFF;
+                self.i_reg = nnn;
+            }
+            // JP, V0, addr -> Jump to location nnn + V0
+            [0xB, ..] => {
+                let nnn = instruction & 0xFFF;
+                self.pc = nnn + self.v_reg[0] as u16;
+            }
+            // RND Vx, byte -> Set Vx = random_byte AND kk;
+            [0xC, x, ..] => {
+                let kk = (instruction & 0xFF) as u8;
+                self.v_reg[x as usize] = rand::random::<u8>() & kk;
+            }
+            // DRW Vx, Vy, nibble -> Display the n-byte sprite starting
+            // at memory location I at (Vx, Vy), SET VF = collision
+            [0xD, x, y, n] => {
+                let (x, y) = (x as usize, y as usize);
+                let vx = self.v_reg[x];
+                let vy = self.v_reg[y];
+                let i = self.i_reg as usize;
+
+                for y in 0..n {
+                    let pixel = self.ram[i + y as usize];
+                    for x in 0..8 {
+                        let msb = 0x80; // most significant bit of the pixel
+                        if (pixel & (msb >> x)) != 0 {
+                            let (d_width, d_height) = (DISPLAY_WIDTH as u8, DISPLAY_HEIGHT as u8);
+                            let wrapped_x = (vx + x) % d_width;
+                            let wrapped_y = (vy + y) % d_height;
+                            // index of the pixel
+                            let idx = (wrapped_x + wrapped_y * d_height) as usize;
+                            self.display[idx] ^= true;
+                            self.v_reg[0xF] = (!self.display[idx]) as u8;
+                        }
+                    }
+                }
+            }
+            // SKP Vx -> Skip next instruction if key with value of Vx is pressed
+            [0xE, x, 9, 0xE] => {
+                if self.keys[self.v_reg[x as usize] as usize] {
+                    self.pc += 2;
+                }
+            }
+            // SKP Vx -> Skip next instruction if key with value of Vx is not pressed
+            [0xE, x, 0xA, 1] => {
+                if !self.keys[self.v_reg[x as usize] as usize] {
+                    self.pc += 2;
+                }
+            }
+            // LD Vx, DT -> Set Vx = delay timer value
+            [0xF, x, 0, 7] => {
+                self.v_reg[x as usize] = self.dt;
+            }
+            // LD, Vx, K -> Wait for a key press, store the value of the key in Vx
+            [0xF, x, 0, 0xA] => {
+                let mut pressed = false;
+                for (idx, key) in self.keys.into_iter().enumerate() {
+                    if key {
+                        self.v_reg[x as usize] = idx as u8;
+                        pressed = true;
+                        break;
+                    }
+                }
+
+                // Continue wating for the key press
+                if !pressed {
+                    self.pc -= 2;
+                }
+            }
+            // LD DT, Vx -> Set delay timer = Vx
+            [0xF, x, 1, 5] => {
+                self.dt = self.v_reg[x as usize];
+            }
+            // LD ST, Vx -> Set sound timer = Vx
+            [0xF, x, 1, 5] => {
+                self.st = self.v_reg[x as usize];
+            }
+            // ADD I, Vx -> Set i_reg = i_reg + Vx
+            [0xF, x, 1, 0xE] => {
+                self.i_reg += self.v_reg[x as usize] as u16;
+            }
+            // LD F, Vx -> Set i_reg = location of sprite for digit Vx
+            [0xF, x, 2, 9] => {
+                self.i_reg = (self.v_reg[x as usize] * 0x5) as u16;
+            }
+            // LD B, Vx -> Store BCD representation of Vx 
+            // in memory locations  I, I + 1, and I + 2
+            [0xF, x, 5, 5] => {
+                let i = self.i_reg as usize;
+                let x = x as usize;
+                self.ram[i]  = self.v_reg[x] / 100;
+                self.ram[i + 1]  = (self.v_reg[x] / 10) % 10;
+                self.ram[i + 2]  = self.v_reg[x] % 10; 
+            }
+            // LD [I], Vx -> Store registers V0 through VX
+            // in memory starting at the location  I
+            [0xF, x, 5, 5] => {
+                let i = self.i_reg as usize;
+                self.v_reg
+                    .into_iter()
+                    .enumerate()
+                    .for_each(|(idx, v)| {
+                        self.ram[i + idx] = v;
+                    })
+            }
+            // LD, Vx, [I] -> Read registers V0 through
+            // VX from memory starting at location I 
+            [0xF, x, 6, 5] => {
+                let i = self.i_reg as usize;
+                self.v_reg
+                    .into_iter()
+                    .enumerate()
+                    .for_each(|(idx, v)| {
+                        self.v_reg[i] = self.ram[i + idx];
+                    })
+            }
+            _ => unreachable!("Error. Unknown instruction: 0x{:x}", instruction),
         }
     }
 
